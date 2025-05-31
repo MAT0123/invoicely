99b63475ec5b2eb09d03eb704e66981485bfd4e9
Matthew <matthewaureliustjoa@gmail.com>
2025-05-30 20:52:40 -0400

    Fixed flashiness before render

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,17 @@ import InvoiceForm from './recycle/CreateInvoice';
 import NewClientForm from './recycle/NewClientForm';
 
 import InvoicePDFGenerator from './components/InvoiceGenerator';
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { app } from './lib/firebaseConfig';
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 import InvoiceDashboard from './home/page';
 import AuthForms from './authentication/page';
 export default function Home() {
   const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
+ 
+  useLayoutEffect(() => {
     if (app) {
       const auth = getAuth(app);
       const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -23,15 +24,15 @@ export default function Home() {
       });
       // return unsubscribe()
     }
-  }, []);
-  if (loading) {
+  }, [])
+
+   if (loading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        <p>Loading...</p>
+      <div className="min-h-screen flex items-center justify-center bg-white">
+        <p className='text-[50px]'>Loading...</p>
       </div>
     );
   }
-
   return (
     <div className="bg-white min-h-screen flex items-center justify-center">
       {user ? <InvoiceDashboard /> : <AuthForms />}
