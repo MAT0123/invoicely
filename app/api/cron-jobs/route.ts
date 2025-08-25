db85df65b6b3d6932cfcfff4d7a6731d91c42214
Matthew <matthewaureliustjoa@gmail.com>
2025-08-25 13:52:26 -0400

    Changed private key

--- app/api/cron-jobs/route.ts
+++ app/api/cron-jobs/route.ts
@@ -1,6 +1,5 @@
 import { sendEmail } from '@/app/lib/email';
 import { InvoiceData } from '@/app/types/invoiceTypes';
-import { collection, getDocs } from 'firebase/firestore';
 import { NextRequest, NextResponse } from 'next/server';
 import { generateInvoiceHTML } from './GenerateHtmlPDFTemplate';
 import * as admin from "firebase-admin";
@@ -8,7 +7,7 @@ import * as admin from "firebase-admin";
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     })
 });
