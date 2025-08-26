ecb9e122da7a01a9f44d67baadf4edb76e0d089a
Matthew <matthewaureliustjoa@gmail.com>
2025-08-26 10:33:04 -0400

    Change finish route

--- app/api/register/finish/route.ts
+++ app/api/register/finish/route.ts
@@ -1,46 +1,46 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import * as SimpleWebAuthnServer from '@simplewebauthn/server'
-// import base64url from 'base64url'
+import { NextRequest, NextResponse } from "next/server";
+import * as SimpleWebAuthnServer from '@simplewebauthn/server'
+import base64url from 'base64url'
 
-// const rp = "www.invoicely.matthewautjoa.tech"
-// let fakeMemoryForChallenge: Record<string, string> = {}
-// export const POST = async (req: NextRequest): NextResponse => {
-//     const username = req.headers.get("username") as string || ""
+const rp = "www.invoicely.matthewautjoa.tech"
+let fakeMemoryForChallenge: Record<string, string> = {}
+export const POST = async (req: NextRequest): Promise<NextResponse<unknown>> => {
+    const username = req.headers.get("username") as string || ""
 
-//     if (username === "") {
-//         return NextResponse.json({
-//             error: 'Username required'
-//         }, {
-//             status: 400,
-//         })
-//     }
-//     let verification;
-//     try {
-//         verification = await SimpleWebAuthnServer.verifyRegistrationResponse({
-//             response: req.body.data,
-//             expectedChallenge: challenges[username],
-//             expectedOrigin: expectedOrigin
-//         });
-//     } catch (error) {
-//         console.error(error);
-//         return NextResponse.json({
-//             error
-//         }, { status: 400 })
-//     }
-//     const { verified, registrationInfo } = verification;
-//     if (verified) {
-//         users[username] = registrationInfo;
-//         return res.status(200).send(true);
-//     }
+    if (username === "") {
+        return NextResponse.json({
+            error: 'Username required'
+        }, {
+            status: 400,
+        })
+    }
+    // let verification;
+    // try {
+    //     verification = await SimpleWebAuthnServer.verifyRegistrationResponse({
+    //         response: req.body.data,
+    //         expectedChallenge: challenges[username],
+    //         expectedOrigin: expectedOrigin
+    //     });
+    // } catch (error) {
+    //     console.error(error);
+    //     return NextResponse.json({
+    //         error
+    //     }, { status: 400 })
+    // }
+    // const { verified, registrationInfo } = verification;
+    // if (verified) {
+    //     users[username] = registrationInfo;
+    //     return res.status(200).send(true);
+    // }
 
-//     return NextResponse.json({
-//         status: "SUCCESS"
-//     }, { status: 200 })
-// }
+    return NextResponse.json({
+        status: "SUCCESS"
+    }, { status: 200 })
+}
 
-// function getNewChallenge() {
-//     return Math.random().toString(36).substring(2);
-// }
-// function convertChallenge(challenge: string) {
-//     return btoa(challenge).replaceAll('=', '');
-// }
+function getNewChallenge() {
+    return Math.random().toString(36).substring(2);
+}
+function convertChallenge(challenge: string) {
+    return btoa(challenge).replaceAll('=', '');
+}
